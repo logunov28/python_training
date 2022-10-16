198e7997ebb316be71385bf669c61fa01fb3070e
logunov28 <79602109+logunov28@users.noreply.github.com>
2022-10-16 11:33:13 +0300

    task4 refactored
    Оставил 1 файл applicaton, выделил SessionHelper

--- test/test_add_contact.py
+++ test/test_add_contact.py
@@ -1,7 +1,7 @@
 # -*- coding: utf-8 -*-
 import pytest
-from contact import Contact
-from app_contact import Application
+from model.contact import Contact
+from fixture.application import Application
 
 
 @pytest.fixture()
@@ -12,11 +12,11 @@ def app(request):
 
 def test_add_contact(app):
     app.open_home_page()
-    app.login(username="admin", password="secret")
+    app.session.login(username="admin", password="secret")
     app.open_add_new_page()
     app.add_contact(Contact(firstname="Kurt", middlename="Donald", lastname="Kobain", nickname="idk", title="idk",
                                  company="Nirvana", address="Aberdeen USA", home_phone="+1999999", mobile_phone="None", work_phone="None", fax="None",
                                  email="kobain@bk.biz", email2="kobain@bk.org", email3="kobain@bk.com", homepage="nirvana.com",
                                  birthday="4", month_of_birth="March", year_of_birth="1967", anniversary_day="10", anniversary_month="October", anniversary_year="2022", address2="New York",
                                  phone2="25", notes="still alive"))
-    app.logout()
+    app.session.logout()
--- test/test_add_group.py
+++ test/test_add_group.py
@@ -1,7 +1,7 @@
 # -*- coding: utf-8 -*-
 import pytest
-from group import Group
-from app_group import Application
+from model.group import Group
+from fixture.application import Application
 
 
 @pytest.fixture()
@@ -12,18 +12,18 @@ def app(request):
 
 def test_add_group(app):
     app.open_home_page()
-    app.login(username="admin", password="secret")
+    app.session.login(username="admin", password="secret")
     app.open_groups_page()
     app.create_group(Group(name="First_group", header="This is the first group.",
                       footer="I mean this group was created earlier than others."))
     app.return_to_groups_page()
-    app.logout()
+    app.session.logout()
 
 def test_add_empty_group(app):
     app.open_home_page()
-    app.login(username="admin", password="secret")
+    app.session.login(username="admin", password="secret")
     app.open_groups_page()
     app.create_group(Group(name="", header="", footer=""))
     app.return_to_groups_page()
-    app.logout()
+    app.session.logout()
 
