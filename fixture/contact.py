e3488ce1349910934b2745316c4865c057053625
logunov28 <79602109+logunov28@users.noreply.github.com>
2022-11-13 10:49:51 +0300

    По видео к разделу 5

--- fixture/contact.py
+++ fixture/contact.py
@@ -1,3 +1,5 @@
+import re
+
 from selenium.webdriver.support.ui import Select
 from model.contact import Contact
 
@@ -162,11 +164,52 @@ class ContactHelper:
             self.open_home_page()
             self.contact_cache = []
             for element in wd.find_elements_by_css_selector("tr[name = entry]"):
+                cells = element.find_elements_by_tag_name("td")
                 lastname = element.find_element_by_css_selector('td:nth-child(2)').text
                 firstname = element.find_element_by_css_selector('td:nth-child(3)').text
                 id = element.find_element_by_name("selected[]").get_attribute("value")
-                self.contact_cache.append(Contact(lastname=lastname, firstname=firstname, id=id))
+                all_phones = cells[5].text
+                self.contact_cache.append(Contact(lastname=lastname, firstname=firstname, id=id, all_phones_from_home_page=all_phones))
         return list(self.contact_cache)
+
+    def open_contact_to_edit_by_index(self, index):
+        wd = self.app.wd
+        self.open_home_page()
+        row = wd.find_elements_by_name("entry")[index]
+        cell = row.find_elements_by_tag_name("td")[7]
+        cell.find_element_by_tag_name("a").click()
+
+    def open_contact_view_by_index(self, index):
+        wd = self.app.wd
+        self.open_home_page()
+        row = wd.find_elements_by_name("entry")[index]
+        cell = row.find_elements_by_tag_name("td")[6]
+        cell.find_element_by_tag_name("a").click()
+
+    def get_contact_info_from_edit_page(self, index):
+        wd = self.app.wd
+        self.open_contact_to_edit_by_index(index)
+        firstname = wd.find_element_by_name("firstname").get_attribute('value')
+        lastname = wd.find_element_by_name("lastname").get_attribute('value')
+        id = wd.find_element_by_name("id").get_attribute('value')
+        home_phone = wd.find_element_by_name("home").get_attribute('value')
+        work_phone = wd.find_element_by_name("work").get_attribute('value')
+        mobile_phone = wd.find_element_by_name("mobile").get_attribute('value')
+        phone2 = wd.find_element_by_name("phone2").get_attribute('value')
+        return Contact(firstname=firstname, lastname=lastname, id=id, home_phone=home_phone, work_phone=work_phone, mobile_phone=mobile_phone, phone2=phone2)
+
+    def get_contact_from_view_page(self, index):
+        wd = self.app.wd
+        self.open_contact_view_by_index(index)
+        text = wd.find_element_by_id("content").text
+        home_phone = re.search("H: (.*)", text).group(1)
+        work_phone = re.search("W: (.*)", text).group(1)
+        mobile_phone = re.search("M: (.*)", text).group(1)
+        phone2 = re.search("P: (.*)", text).group(1)
+        return Contact(home_phone=home_phone, work_phone=work_phone,
+                       mobile_phone=mobile_phone, phone2=phone2)
+
+
 
 
 
--- model/contact.py
+++ model/contact.py
@@ -3,7 +3,8 @@ from sys import maxsize
 class Contact:
 
     def __init__(self, firstname=None, middlename=None, lastname=None, nickname=None, title=None, company=None, address=None, home_phone=None, mobile_phone=None, work_phone=None, fax=None,
-                 email=None, email2=None, email3=None, homepage=None, birthday=None, month_of_birth=None, year_of_birth=None, anniversary_day=None, anniversary_month=None, anniversary_year=None, address2=None, phone2=None, notes=None, id=None):
+                 email=None, email2=None, email3=None, homepage=None, birthday=None, month_of_birth=None, year_of_birth=None, anniversary_day=None, anniversary_month=None, anniversary_year=None, address2=None, phone2=None, notes=None, id=None,
+                 all_phones_from_home_page=None):
         self.firstname = firstname
         self.middlename = middlename
         self.lastname = lastname
@@ -29,6 +30,7 @@ class Contact:
         self.phone2 = phone2
         self.notes = notes
         self.id = id
+        self.all_phones_from_home_page = all_phones_from_home_page
 
 
     def __repr__(self):
