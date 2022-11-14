cff44ce63398a450b16a90340b7a1119c058a749
logunov28 <79602109+logunov28@users.noreply.github.com>
2022-11-14 17:39:53 +0300

    task14v2
    Объединил тесты по инфо о контактах.

--- test/test_contact_info.py
+++ test/test_contact_info.py
@@ -1,5 +1,19 @@
 import re
 
+
+def test_names_on_home_page(app):
+    contact_from_home_page = app.contact.get_contact_list()[0]
+    contact_from_edit_page = app.contact.get_contact_info_from_edit_page(0)
+    assert contact_from_home_page.firstname == contact_from_edit_page.firstname
+    assert contact_from_home_page.lastname == contact_from_edit_page.lastname
+
+
+def test_emails_on_home_page(app):
+    contact_from_home_page = app.contact.get_contact_list()[0]
+    contact_from_edit_page = app.contact.get_contact_info_from_edit_page(0)
+    assert contact_from_home_page.all_emails_from_home_page == merge_emails_like_on_home_page(contact_from_edit_page)
+
+
 def test_phones_on_home_page(app):
     contact_from_home_page = app.contact.get_contact_list()[0]
     contact_from_edit_page = app.contact.get_contact_info_from_edit_page(0)
@@ -14,8 +28,14 @@ def test_phones_on_contact_view_page(app):
     assert contact_from_home_page.mobile_phone == contact_from_edit_page.mobile_phone
     assert contact_from_home_page.phone2 == contact_from_edit_page.phone2
 
+
 def clear(s):
     return re.sub("[() -]", "", s)
 
+
 def merge_phones_like_on_home_page(contact):
     return "\n".join(filter(lambda x: x != "", map(lambda x: clear(x), filter(lambda x: x is not None, [contact.home_phone, contact.mobile_phone, contact.work_phone, contact.phone2]))))
+
+
+def merge_emails_like_on_home_page(contact):
+    return "\n".join(filter(lambda x: x != "", filter(lambda x: x is not None, [contact.email, contact.email2, contact.email3])))
