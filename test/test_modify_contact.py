d47662df72c35b54b9a8c13e939516037c30fd65
logunov28 <79602109+logunov28@users.noreply.github.com>
2022-12-08 00:50:57 +0300

    task20 fix3

--- test/test_modify_contact.py
+++ test/test_modify_contact.py
@@ -12,6 +12,11 @@ def test_modify_contact(app, db, json_contacts, check_ui):
     app.contact.modify_contact_by_id(random_contact.id, contact)
     new_contacts = db.get_contact_list()
     assert len(old_contacts) == len(new_contacts)
+    for i in range(len(old_contacts)):
+        if old_contacts[i].id == random_contact.id:
+            old_contacts[i] = contact
+            old_contacts[i].id = random_contact.id
+    assert sorted(old_contacts, key=Contact.id_or_max) == sorted(new_contacts, key=Contact.id_or_max)
     if check_ui:
         assert sorted(new_contacts, key=Contact.id_or_max) == sorted(app.contact.get_contact_list(),
                                                                      key=Contact.id_or_max)
