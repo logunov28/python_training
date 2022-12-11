fc3a874b1cd55fefe9d8390d23fc8e1d50b7b4f5
logunov28 <79602109+logunov28@users.noreply.github.com>
2022-12-11 23:44:19 +0300

    Task21

--- test/test_contact_info.py
+++ test/test_contact_info.py
@@ -1,22 +1,27 @@
+from model.contact import Contact
 import re
 
+def test_info_on_home_page(app, db):
+    db_contacts = db.get_contact_list()
+    for i in db_contacts:
+        i.all_emails_from_home_page = merge_emails_like_on_home_page(i)
+        i.all_phones_from_home_page = merge_phones_like_on_home_page(i)
+    db_contacts = sorted(db_contacts, key=Contact.id_or_max)
+    contacts_from_home_page = sorted(app.contact.get_contact_list(), key=Contact.id_or_max)
+    assert len(db_contacts) == len(contacts_from_home_page)
+    assert db_contacts == contacts_from_home_page
+    for i in range(len(db_contacts)):
+        print(str(i))
+        print(db_contacts[i])
+        print(contacts_from_home_page[i])
 
-def test_info_on_home_page(app):
-    contact_from_home_page = app.contact.get_contact_list()[0]
-    contact_from_edit_page = app.contact.get_contact_info_from_edit_page(0)
-    assert contact_from_home_page.firstname == contact_from_edit_page.firstname
-    assert contact_from_home_page.lastname == contact_from_edit_page.lastname
-    assert contact_from_home_page.all_emails_from_home_page == merge_emails_like_on_home_page(contact_from_edit_page)
-    assert contact_from_home_page.all_phones_from_home_page == merge_phones_like_on_home_page(contact_from_edit_page)
-
-
-def test_phones_on_contact_view_page(app):
-    contact_from_home_page = app.contact.get_contact_from_view_page(0)
-    contact_from_edit_page = app.contact.get_contact_info_from_edit_page(0)
-    assert contact_from_home_page.home_phone == contact_from_edit_page.home_phone
-    assert contact_from_home_page.work_phone == contact_from_edit_page.work_phone
-    assert contact_from_home_page.mobile_phone == contact_from_edit_page.mobile_phone
-    assert contact_from_home_page.phone2 == contact_from_edit_page.phone2
+    for i in range(len(db_contacts)):
+        assert db_contacts[i].id == contacts_from_home_page[i].id
+        assert db_contacts[i].firstname == contacts_from_home_page[i].firstname
+        assert db_contacts[i].lastname == contacts_from_home_page[i].lastname
+        assert db_contacts[i].address == contacts_from_home_page[i].address
+        assert db_contacts[i].all_phones_from_home_page == contacts_from_home_page[i].all_phones_from_home_page
+        assert db_contacts[i].all_emails_from_home_page == contacts_from_home_page[i].all_emails_from_home_page
 
 
 def clear(s):
